d254342f9ac25fcfb62c02faad9254627e2c230d
Nick Howes <N.Howes@warwick.ac.uk>
2022-08-21 22:36:14 +0100

    Force JSON response.

--- src/main/kotlin/uk/ac/warwick/cloud/prototype/PrototypeApplication.kt
+++ src/main/kotlin/uk/ac/warwick/cloud/prototype/PrototypeApplication.kt
@@ -5,6 +5,7 @@ import org.springframework.boot.runApplication
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.MediaType
 
 @SpringBootApplication
 @RestController
@@ -16,7 +17,7 @@ class PrototypeApplication {
 	@Value("\${app.deployment}")
 	lateinit var deployment: String
 
-	@GetMapping("/")
+	@GetMapping("/", produces = [ MediaType.APPLICATION_JSON_VALUE ])
 	fun home() = mapOf(
 		"message" to message,
 		"deployment" to deployment
